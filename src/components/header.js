eb2fd8ece6b1904a31eb6e437113223011ddfc29
Ray <lhy131120@gmail.com>
2025-11-06 17:36:46 +0800

    init ex

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+export function generateMainNav() {
+  const mainNavs = [
+		{ section: "床墊優勢", path: "#adv" },
+		{ section: "好評推薦", path: "#comments" },
+		{ section: "運送方式", path: "#transportation" },
+		{ section: "後台訂單", path: "javascript:;" },
+	];
+
+  const mainNav = document.querySelector('#mainNav');
+
+  // set class
+  const mainNavClass = "block py-5 text-2xl leading-normal hover:text-accent transition-all duration-500 font-medium";
+
+  mainNavs.forEach(nav => {
+    let a = document.createElement('a')
+    a.setAttribute('href', `${nav.path}`);
+    a.setAttribute('class', mainNavClass);
+    a.textContent = nav.section;
+    mainNav.appendChild(a);
+  })
+}
